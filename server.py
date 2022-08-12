bebbbb19c16bf97897f8acf9195a8d4c359abbb6
hasnainkhure <86131538+hasnainkhure@users.noreply.github.com>
2022-08-12 02:01:29 -0400

    updates with form and ipfs

--- server.py
+++ server.py
@@ -1,13 +1,87 @@
 import os
-from flask import Flask, render_template, request, send_from_directory
+import json
+from dotenv import load_dotenv
+import requests
+from flask import Flask, render_template, request, send_from_directory, url_for
+from flask_uploads import UploadSet, IMAGES, configure_uploads
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField, StringField
+from wtforms.validators import DataRequired
+
 # from generative_art import *
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/Images/'
 
-IMAGE_FOLDER = "static/Images"
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
 
-app.config['IMAGE_FOLDER'] = IMAGE_FOLDER
+class UploadForm(FlaskForm):
+    photo = FileField(
+        validators=[
+            FileAllowed(photos, 'Only images are allowed'),
+            FileRequired('File field should not be empty')
+        ]
+    )
+    name = StringField('What is your token called?', validators=[DataRequired()])
+    submit = SubmitField('Upload')
+
+
+json_headers = {
+    "Content-Type": "application/json",
+    "pinata_api_key": os.getenv("PINATA_API_KEY"),
+    "pinata_secret_api_key": os.getenv("PINATA_SECRET_API_KEY"),
+}
+
+file_headers = {
+    "pinata_api_key": os.getenv("PINATA_API_KEY"),
+    "pinata_secret_api_key": os.getenv("PINATA_SECRET_API_KEY"),
+}
+
+def convert_data_to_json(content):
+    data = {"pinataOptions": {"cidVersion": 1}, "pinataContent": content}
+    return json.dumps(data)
+
+def pin_file_to_ipfs(data):
+    r = requests.post(
+        "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        files={'file': data},
+        headers=file_headers
+    )
+    print(r.json())
+    ipfs_hash = r.json()["IpfsHash"]
+    return ipfs_hash
+
+def pin_json_to_ipfs(json):
+    r = requests.post(
+        "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+        data=json,
+        headers=json_headers
+    )
+    print(r.json())
+    ipfs_hash = r.json()["IpfsHash"]
+    return ipfs_hash
+
+def pin_artwork(artwork_name, artwork_file):
+    # Pin the file to IPFS with Pinata
+    ipfs_file_hash = pin_file_to_ipfs(artwork_file.getvalue())
+
+    # Build a token metadata file for the artwork
+    token_json = {
+        "name": artwork_name,
+        "image": ipfs_file_hash
+    }
+    json_data = convert_data_to_json(token_json)
+
+    # Pin the json to IPFS with Pinata
+    json_ipfs_hash = pin_json_to_ipfs(json_data)
+
+    return json_ipfs_hash, token_json
+
+nft_url = ''
 
 @app.route("/")
 @app.route("/home/")
@@ -18,20 +92,58 @@ def home():
 def generate_art():
     return render_template("generate-art.html")
 
-@app.route("/mint-nft/")
+@app.route('/images/<filename>')
+def get_file(filename):
+    return send_from_directory(app.config['UPLOADED_PHOTOS_DEST'],
+                               filename)
+
+@app.route('/mint-nft/')
+def mint_nft_page():
+    form = UploadForm()
+    return render_template("nft.html", form=form)
+
+@app.route("/mint-nft/ipfs-file", methods=['POST'])
 def nft():
-    return render_template("nft.html")
+    form = UploadForm()
+    if form.validate_on_submit():
+        filename = photos.save(form.photo.data)
+        file_url = url_for('get_file', filename=filename)
+        nft_name = form.name.data
+        nft_ipfs_hash,token_json = pin_artwork(
+            nft_name,
+            file_url)
+        
+        nft_uri = f"ipfs.io/ipfs/{nft_ipfs_hash}"
+    else:
+        file_url = None
+    return render_template("nft.html", form=form, file_url=file_url, nft_uri=nft_uri, token_json=token_json)
+
+# @app.route("/ipfs-file/", methods=['GET', 'POST'])
+# def push_to_pinata():
+#     nft_name = data['nft_name']
+#     file = nft_url
+#     nft_ipfs_hash,token_json = pin_artwork(
+#         nft_name,
+#         file)
+    
+#     nft_uri = f"ipfs.io/ipfs/{nft_ipfs_hash}"
+#     return render_template("nft.html", nft_uri=nft_uri, token_json=token_json)
+
+# def push_to_pinata():
+#     nft_name = "DV"
+#     nft_ipfs_hash,token_json = pin_nft(
+#         nft_name,
+#         file, 
+#         date_of_creation = creation_date, 
+#         details=about_the_nft)
+
+#     nft_uri = f"ipfs.io/ipfs/{nft_ipfs_hash}"
+#     return render_template("nft.html")
+
 
 @app.route("/images/")
 def images():
     return render_template("images.html")
-
-@app.route('/images/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['IMAGE_FOLDER'],
-                               filename)
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/app.js
+++ static/app.js
@@ -1,20 +1,66 @@
-let provider;
-let accounts;
-//const web3 = new Web3();
-let accountAddress = "";
-let signer;
-let web3_flag = 0;
-let img_url;
-let tweet_url;
-let data;
-let ChainId;
-let nft_msg_before_loading;
-let tweetIdFromUrl;
-let smartcontract_addr;
-let transation_hash;
-let ChainId_db;
-//const Web3 = require("web3");
+// let provider;
+// let accounts;
+// //const web3 = new Web3();
+// let accountAddress = "";
+// let signer;
+// let web3_flag = 0;
+// let img_url;
+// let tweet_url;
+// let data;
+// let ChainId;
+// let nft_msg_before_loading;
+// let tweetIdFromUrl;
+// let smartcontract_addr;
+// let transation_hash;
+// let ChainId_db;
+// //const Web3 = require("web3");
 
+// function construct_Web3_Validation()
+// { 
+//     console.log(web3_flag);
+//     console.log(url_web3);    
+//     if (web3_flag == 1)
+//     {
+//         document.getElementById("web3_msg").innerHTML = document.getElementById("web3_msg").innerHTML + url_web3;
+//     }
+// }
+
+// if(ChainId == 4){ 
+//     smartcontract_addr = data['contract_address'];
+//     var myContract = await new window.web3.eth.Contract(abi, data['contract_address'], {from: accountAddress, // default from address
+//     gasPrice: '20000000000' // default gas price in wei, 20 gwei in this case
+// });
+//     console.log("[[11]]");
+//     //console.log(myContract.options);
+// myContract.methods.mint(accountAddress, tokenUri).send({from: accountAddress})
+// .then(function(receipt)                                  
+// {
+//     console.log("[[12]]");
+// // receipt can also be a new contract instance, when coming from a "contract.deploy({...}).send()"
+//     console.log(receipt);
+//     console.log("[[13]]");    
+//     document.getElementById("nft_msg2").innerHTML = "<h4> NFT Minted! </h4><p style='text-align:left;'>Congratulations! <br>Here is your Transaction Hash:<br><a target='_blank'  href='https://rinkeby.etherscan.io/tx/"+String(receipt['transactionHash']) + "'>" + String(receipt['transactionHash'])+"</a></p>";
+//     transation_hash = receipt['transactionHash'];
+//     ChainId_db = "4";
+//     writeNFTToDB("0 - Minted");
+                                
+// })
+// .catch((error) => {
+//       if (error['code'] === 4001) {
+//         // EIP-1193 userRejectedRequest error
+//         console.log('Please connect to MetaMask.');
+//            document.getElementById("nft_msg2").innerHTML = "<h4> NFT could not be minted!</h4><p style='text-align:left;'>Looks like you <i>rejected</i> transaction signature.<br>So we cannot Mint your NFT. If you still want to Mint your NFT, please try again?</p>"; 
+//                   transation_hash = "Not Minted";
+//     ChainId_db = "4";          
+//               writeNFTToDB("1 - Transaction Rejected");
+//       } else {
+//         console.error(error);
+//                   transation_hash = "Not Minted";
+//     ChainId_db = "4";          
+//               writeNFTToDB("2 - Transaction Failed - Reason:"+ String(error).slice(0,25));          
+//       }
+//     });
+// }
 
 window.userWalletAddress = null
 const connectWallet = document.getElementById('connectWallet')
